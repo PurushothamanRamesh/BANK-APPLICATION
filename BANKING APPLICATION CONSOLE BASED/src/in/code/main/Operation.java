0571fef4c0b4bd685f348c8da9c9f99ccd7fd667
purus <purus@PURUSHOTH>
2023-03-11 13:01:54 +0530

    Completed

--- BANKING APPLICATION CONSOLE BASED/src/in/code/main/Operation.java
+++ BANKING APPLICATION CONSOLE BASED/src/in/code/main/Operation.java
@@ -9,7 +9,7 @@ public class Operation {
 	    System.out.println(" ***Banking System Application***");
 	    System.out.println(" -----------------------------------------------------------------------------------------------------------------------------------------------------------------");
 	   
-	    System.out.println(" 1. Create a new account  \n 2. Check Balance\n 3. Deposit the amount \n 4. Withdraw the amount  \n 5. Watch demo account \n 6. Exit  \n   Enter your choice:: "); 
+	    System.out.println(" 1. Create a new account  \n 2. Check Balance\n 3. Deposit the amount \n 4. Withdraw the amount  \n 5. Watch demo account \n 6. Exit  \n\nENTER YOUR CHOISE :: "); 
 	    int key=scan.nextInt();
 	    operation( key);
 	    
@@ -21,35 +21,50 @@ public class Operation {
 			process bankprocess = new process();
 			 switch (key) {
 				case 1:
+				    System.out.println(" -----------------------------------------------------------------------------------------------------------------------------------------------------------------");
 					bankprocess.openAccount();
-					System.out.println("MAIN PAGE_:: PRESS 1 ::");
+					System.out.println(" -----------------------------------------------------------------------------------------------------------------------------------------------------------------");
+					System.out.println();
+				    System.out.println("MAIN PAGE_:: PRESS 1 ::");
 					if (scan.nextInt()==1) 
 						bankinfo();
 					break;
 				case 2:
-					System.out.println("Your name is :: "+bank.getName());
-					System.out.println("Your balance  is :: "+bank.getBalance());
-					System.out.println("THANKS FOR BALANCE CHECKING ");
+				    System.out.println(" -----------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+					bankprocess.checkbalance();
+					System.out.println();
+					System.out.println(" -----------------------------------------------------------------------------------------------------------------------------------------------------------------");
 					System.out.println("MAIN PAGE_:: PRESS 1 ::");
 					if (scan.nextInt()==1) 
 						bankinfo();
 					break;
 				case 3:
-					System.out.println("WELCOME TO DEPOSITE PAGE ");
+				    System.out.println(" -----------------------------------------------------------------------------------------------------------------------------------------------------------------");
+				    System.out.println();
+					System.out.println("---------WELCOME TO DEPOSITE PAGE---------------- ");
 					bankprocess.deposite();
+					System.out.println();
+				    System.out.println(" -----------------------------------------------------------------------------------------------------------------------------------------------------------------");
 					System.out.println("MAIN PAGE_:: PRESS 1 ::");
 					if (scan.nextInt()==1) 
 						bankinfo();
 					break;
 				case 4:
-					System.out.println("MAIN PAGE_:: PRESS 1 ::");
+				    System.out.println(" -----------------------------------------------------------------------------------------------------------------------------------------------------------------");
 					bankprocess.withdraw();
+					System.out.println();
+					System.out.println(" -----------------------------------------------------------------------------------------------------------------------------------------------------------------");
+					System.out.println("MAIN PAGE_:: PRESS 1 ::");
 					if (scan.nextInt()==1) 
 						bankinfo();
 					break;
 				case 5:
+				    System.out.println(" -----------------------------------------------------------------------------------------------------------------------------------------------------------------");
+				    bankprocess.demoaccount();
+				    System.out.println();
+					System.out.println(" -----------------------------------------------------------------------------------------------------------------------------------------------------------------");
 					System.out.println("MAIN PAGE_:: PRESS 1 ::");
-					bankprocess.demoaccount();
 					if (scan.nextInt()==1) 
 						bankinfo();
 					break;
--- BANKING APPLICATION CONSOLE BASED/src/in/code/main/process.java
+++ BANKING APPLICATION CONSOLE BASED/src/in/code/main/process.java
@@ -4,10 +4,17 @@ import java.util.Scanner;
 
 public class process {
  Scanner sc = new Scanner(System.in);  
- BANKDETAILS bank1 = new BANKDETAILS();
-    
+ static BANKDETAILS bank1 = new BANKDETAILS();
+
+    static {
+    	bank1.setAccno("87643682497638");
+    	bank1.setName("MANI DEMO");
+    	bank1.setAcc_type("SAVING");
+    	bank1.setBalance(986146187);
+  
+    }
     public void openAccount() {  
-        System.out.print("Enter  Account No: ");  
+        System.out.print("Enter Account No: ");  
         bank1.setAccno(sc.next()); 
         System.out.print("Enter Account type: ");  
         bank1.setAcc_type(sc.next());
@@ -17,22 +24,22 @@ public class process {
         bank1.setBalance(sc.nextLong());
         
         System.out.println("------YOUR ACCOUNT DETAILS IS -------");
-        System.out.println("Name of account holder: " + bank1.getName());  
-        System.out.println("Account no.: " + bank1.getAccno());  
-        System.out.println("Account type: " + bank1.getAcc_type());  
-        System.out.println("Balance: " + bank1.getBalance());  
+        System.out.println("Name of account holder :: " + bank1.getName());  
+        System.out.println("Account no             :: " + bank1.getAccno());  
+        System.out.println("Account type           :: " + bank1.getAcc_type());  
+        System.out.println("Balance                :: " + bank1.getBalance());  
         
     } 
     public void demoaccount() {
     	int  demobalance=50000;
     	System.out.println("Name of account holder :: " + "Demo user");  
-        System.out.println("Account no :: " + "9878987898");  
-        System.out.println("Account type: " + "demo");  
-        System.out.println("Balance: " + demobalance);  
+        System.out.println("Account no             :: " + "9878987898");  
+        System.out.println("Account type           :: " + "demo");  
+        System.out.println("Balance                :: " + demobalance);  
     	
     }
     public void deposite(){
-    	System.out.println("Enter the Amount you want to deposit:");
+    	System.out.println("Enter the Amount you want to deposit ::");
 		int deposit =sc.nextInt();
 		int amount =(int) ((bank1.getBalance())+deposit);
 		bank1.setBalance(amount);
@@ -49,14 +56,22 @@ public class process {
 		{
 			bank1.setBalance(bank1.getBalance()-withdraw);
 			System.out.println(" "+ withdraw+" is withdrawn from your Account");
-			System.out.println("Current Available Balance is Rs."+ bank1.getBalance());
+			System.out.println("Current Available Balance is Rs  ::"+ bank1.getBalance());
 		}
 		else
 		{
 			System.out.println("Low Balance");
-			System.out.println("Current Available Balance is Rs."+ bank1.getBalance());
+			System.out.println("Current Available Balance is Rs  ::"+ bank1.getBalance());
 		}
     }
+	public void checkbalance() {
+		System.out.println("Your name is           :: "+bank1.getName());
+        System.out.println("Account no             :: " + bank1.getAccno());  
+        System.out.println("Account type           :: " + bank1.getAcc_type());  
+	    System.out.println("Balance                :: " + bank1.getBalance());  
+		System.out.println("THANKS FOR BALANCE CHECKING ");
+		
+	}
     
 
 }
